b1d44c047e8bed335f8218b6d40f4e07f945992f
𓆩𝐀𝐘𝐔𝐒𝐇𓆪 ♡ 𝐗ᴅ🪽 <105988266+Ayushjha9988@users.noreply.github.com>
2024-12-12 08:42:25 +0530

    Update reload.py

--- DAXXMUSIC/plugins/tools/reload.py
+++ DAXXMUSIC/plugins/tools/reload.py
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/DISTROYERx7")
+                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/DESTR0YERX7")
                  ]
             ]
          ),
